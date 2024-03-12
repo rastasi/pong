7c6b8ca4c1d0238e0c00e7499005a6069c9a5760
Zsolt Tasnadi <rastasi@gmail.com>
2024-03-12 14:41:35 +0100

    main_menu

--- scenes/playground/Playground.gd
+++ scenes/playground/Playground.gd
@@ -10,38 +10,58 @@ func start():
 	add_child(ball)
 
 func set_score():
-	get_node("Score").text = "{right} - {left}".format({
+	get_node("Score").text = "{left} - {right}".format({
 		"left": left_point,
 		"right": right_point
 	})
+
 
 func new_round(ball):
 	remove_child(ball)
 	set_score()
 	start()
 
+
+func body_is_ball(body):
+	return body.name == "Ball"
+
+
 func _ready():
 	start()
+
 
 func _process(delta):
 	pass
 
-func _on_goal_area_left_body_entered(body):
-	if body.name == "Ball":	
-		left_point += 1
-		new_round(body)
 
-func _on_goal_area_right_body_entered(body):
-	if body.name == "Ball":
+func _on_goal_area_left_body_entered(body):
+	if body_is_ball(body):
 		right_point += 1
 		new_round(body)
 
 
-func _on_racket_left_body_entered(body):
-	if body.name == "Ball":
-		body.linear_velocity = body.linear_velocity * -1
+func _on_goal_area_right_body_entered(body):
+	if body_is_ball(body):
+		left_point += 1
+		new_round(body)
 
+
+func ball_to_racket(body):
+	if body_is_ball(body):
+		var new_linear_velocity = body.linear_velocity * -1
+		
+		#if new_linear_velocity.y == 300 and new_linear_velocity.x == 300:
+		#	new_linear_velocity.x -= 10
+		#	new_linear_velocity.y += 10
+			
+		body.linear_velocity = new_linear_velocity
+		print_debug(new_linear_velocity)
+
+
+func _on_racket_left_body_entered(body):
+	ball_to_racket(body)
 
 func _on_racket_right_body_entered(body):
-	if body.name == "Ball":
-		body.linear_velocity = body.linear_velocity * -1
+	ball_to_racket(body)
+
+
--- scenes/playground/racket/Racket.gd
+++ scenes/playground/racket/Racket.gd
@@ -3,9 +3,6 @@ extends RigidBody2D
 
 const SPEED = 300.0
 
-func _ready():
-	contact_monitor = true
-
 func _physics_process(delta):
 	if Input.is_action_pressed(get_meta("UP_KEY")):
 		move_and_collide(Vector2.UP * SPEED * delta)
