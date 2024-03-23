daf88c489c41b36e875bc2ba85fc5a19ec27609b
Zsolt Tasnadi <rastasi@gmail.com>
2024-03-23 14:51:21 +0100

    main menu, bootsplash tweaks

--- scenes/boot_splash/BootSplash.gd
+++ scenes/boot_splash/BootSplash.gd
@@ -2,9 +2,14 @@ extends Node2D
 
 
 func _ready():
-	await get_tree().create_timer(1.0).timeout # Replace with function body.
+	var base_font_size = 20
+	for i in range(100):
+		await get_tree().create_timer(0.02).timeout
+		var label = get_node("TeletypeGames")
+		label.get_label_settings().font_size = base_font_size + i
+	await get_tree().create_timer(1.5).timeout
 	get_tree().change_scene_to_file("res://scenes/main_menu/MainMenu.tscn")
-
+	
 
 func _process(delta):
 	pass
